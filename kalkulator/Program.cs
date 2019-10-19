0ee4436988db76fd3904ea1333ab1cfff07e0e72
arciomz <kamkoniuk@wp.pl>
2019-10-19 09:20:51 +0200

    Give possibility to exit program

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -16,6 +16,8 @@ namespace kalkulator
                 Console.WriteLine("1. Dodawanie");
                 Console.WriteLine("2. Odejmowanie");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 0)
+                    break;
 
 
             }
